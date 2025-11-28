1390fea12ba7c1a373067d8fcdcfc1a1b4dc2c29
Guruprasaath S <gurukprs@gmail.com>
2025-11-28 09:34:19 +0530

    added img to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_many :post_tags, dependent: :destroy
   has_many :tags, through: :post_tags
   has_many :ratings, dependent: :destroy
+  has_one_attached :image
 
   validates :title, presence: true
   validates :body, presence: true
