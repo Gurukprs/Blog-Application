717620de84e6e2a1bc108d16bbcc364167d22b9f
Guruprasaath S <gurukprs@gmail.com>
2025-12-25 18:24:28 +0530

    Sign up Mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   root to: 'topics#index'
   resources :topics do
     resources :posts do
